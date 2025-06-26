391983cfacf1c0c4468879fe7e9b0adce8a197dc
Ariel Ayaviri <eiddymeister@gmail.com>
2025-06-26 19:18:06 +0000

    ya eata cargado sin embargo reiniciando la app no se ve en la pantalla e

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,12 +2,12 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 export const Logo = ({ className }: { className?: string }) => (
-  <div className={cn("relative", className)}>
+  <div className={cn(className)}>
     <Image
       src="/logo.png"
       alt="DelApp Logo"
-      layout="fill"
-      objectFit="contain"
+      width={80}
+      height={80}
       priority
     />
   </div>
